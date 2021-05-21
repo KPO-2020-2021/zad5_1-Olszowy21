41f82e446b2e56b90cad8835737787350e2046a2
Kamilos21 <262757@student.pwr.edu.pl>
2021-05-21 22:24:00 +0200

    Initial commit

--- app/main.cpp
+++ app/main.cpp
@@ -24,7 +24,7 @@ int main() {
             << "."
             << PROJECT_VERSION_PATCH
             << "."
-            << PROJECT_VERSION_TWEAK //jebie mi z dupy
+            << PROJECT_VERSION_TWEAK //jebie mi z dupy i justynie też
             << std::endl;
   std::system("cat ../LICENSE");
 
